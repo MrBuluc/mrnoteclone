2bbe5b0058908972da0e70ab57fdb07715db1934
U180709061 <hakkicanbuluc@gmail.com>
2022-02-14 15:48:40 +0300

    Settings Page tasarımına devam

--- lib/ui/Settings/SettingsPage.dart
+++ lib/ui/Settings/SettingsPage.dart
@@ -14,9 +14,11 @@ class _SettingsPageState extends State<SettingsPage> {
 
   Settings settings = Settings();
 
-  String passwordStr, showPassword;
+  String passwordStr, showPassword = "";
 
   double ekranYuksekligi, ekranGenisligi;
+
+  bool show = false;
 
   @override
   void initState() {
@@ -42,6 +44,14 @@ class _SettingsPageState extends State<SettingsPage> {
             height: 10,
           ),
           changeColorWidget(),
+          SizedBox(
+            height: 10,
+          ),
+          currentPassword(),
+          SizedBox(
+            height: 10,
+          ),
+          changePassword(),
         ],
       ),
     ));
@@ -115,6 +125,142 @@ class _SettingsPageState extends State<SettingsPage> {
                   );
                 });
           },
+        )
+      ],
+    );
+  }
+
+  Widget currentPassword() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 35),
+          child: Text(
+            "Şuanki Parola:",
+            style: headerStyle7.copyWith(color: Colors.grey.shade900),
+          ),
+        ),
+        Text(
+          show ? passwordStr : showPassword,
+          style: TextStyle(fontSize: 20),
+        ),
+        passwordStr != null
+            ? GestureDetector(
+                child: Icon(show ? Icons.visibility_off : Icons.visibility),
+                onTap: () {
+                  setState(() {
+                    show = !show;
+                  });
+                },
+              )
+            : Container(
+                width: 30,
+              )
+      ],
+    );
+  }
+
+  Widget changePassword() {
+    String bankSelected;
+    return Padding(
+      padding: const EdgeInsets.only(left: 40, right: 40, bottom: 24, top: 12),
+      child: Container(
+        height: 55,
+        width: ekranGenisligi,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(3)),
+            color: Colors.grey.shade400),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+              canvasColor: Colors.grey.shade400,
+              buttonTheme:
+                  ButtonTheme.of(context).copyWith(alignedDropdown: true)),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+              iconEnabledColor: Colors.grey.shade400,
+              items: [""]
+                  .map((String value) => DropdownMenuItem<String>(
+                        value: value,
+                        child: Container(
+                          height: 200,
+                          width: ekranGenisligi - 125,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Parola Değiştir",
+                                style: headerStyle3,
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Form(
+                                child: buildForm(),
+                              )
+                            ],
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              hint: Text(
+                "Parolanı Değiştir",
+                style: headerStyle7,
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  bankSelected = value;
+                });
+              },
+              value: bankSelected,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildForm() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                  hintText: "Parola",
+                  border: OutlineInputBorder()),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              child: Text(
+                "Kaydet",
+                style: headerStyle7,
+              ),
+              style: ElevatedButton.styleFrom(primary: Colors.grey.shade800),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text(
+                "İptal",
+                style: headerStyle4,
+              ),
+              style: ElevatedButton.styleFrom(primary: settings.currentColor),
+              onPressed: () {},
+            )
+          ],
         )
       ],
     );
